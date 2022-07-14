d04f6d91cf98bd766ff21ffffb4b16ed4ab0bb3e
Jose Maria <jmariamendes@uol.com.br>
2022-07-14 20:14:27 -0300

    "Corrigindo endereço e-mail UOL"

--- social/settings.py
+++ social/settings.py
@@ -131,8 +131,10 @@ LOGOUT_REDIRECT_URL = "dwitter:dashboard"
 
 EMAIL_HOST = "smtps.uol.com.br"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = "jmariamendes@uol.com.br"
+EMAIL_HOST_PASSWORD = "ibiuna27"
 EMAIL_USE_TLS = True
 
 
